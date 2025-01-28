d581849f908f02882c3f2edcf1776d86ec8ee28e
timothee <if23b084@technikum.wien.at>
2025-01-28 18:56:48 +0100

    Package Unittests

--- src/test/java/MonsterCardTradingGameTest/service/PackageServiceTest.java
+++ src/test/java/MonsterCardTradingGameTest/service/PackageServiceTest.java
@@ -94,6 +94,49 @@ public class PackageServiceTest {
         assertEquals("zero",createdCards.get(4).getBought());
         assertEquals(1,createdCards.get(4).getPid());
         assertEquals("Water",createdCards.get(4).getElementType());
+    }
+
+    @Test
+    void createPackageTestNoAdmin(){
+        String jdbcUrl = "jdbc:h2:~/mctg;MODE=PostgreSQL;DATABASE_TO_LOWER=TRUE;DEFAULT_NULL_ORDERING=HIGH;INIT=RUNSCRIPT FROM 'classpath:createPackageTest.sql'";
+        UnitOfWork unitOfWork = new UnitOfWork(jdbcUrl);
+        packageRepository = new PackageRepositoryImpl(unitOfWork);
+        packageRepositoryTest = new PackageRepositoryTestImpl(unitOfWork);
+        Card card = new Card();
+        card.setId("ed1dc1bc-f0aa-4a0c-8d43-1402189b33c8");
+        card.setName("WaterGoblin");
+        card.setDamage(10);
+        List<Card> cards = new ArrayList<>();
+        cards.add(card);
+        card = new Card();
+        card.setId("65ff5f23-1e70-4b79-b3bd-f6eb679dd3b5");
+        card.setName("Dragon");
+        card.setDamage(50);
+        cards.add(card);
+        card = new Card();
+        card.setId("55ef46c4-016c-4168-bc43-6b9b1e86414f");
+        card.setName("WaterSpell");
+        card.setDamage(20);
+        cards.add(card);
+        card = new Card();
+        card.setId("f3fad0f2-a1af-45df-b80d-2e48825773d9");
+        card.setName("Ork");
+        card.setDamage(45);
+        cards.add(card);
+        card = new Card();
+        card.setId("8c20639d-6400-4534-bd0f-ae563f11f57a");
+        card.setName("WaterSpell");
+        card.setDamage(25);
+        cards.add(card);
+        String token = "skdjb-mtcgToken";
+
+
+        DataAccessException exception = assertThrows(DataAccessException.class, () -> {
+            packageRepository.createPackage(cards,token);
+            packageRepositoryTest.getCreatedPackageTest();
+        });
+
+        assertTrue(exception.getMessage().contains("you are not an admin"));
 
     }
 }
